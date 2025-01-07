000eb8d02ae2053cf6e879bf97eb04b7530bc28b
Md. Mukaddemu Khan Noman <MNoman@rgbd.local>
2025-01-07 12:38:43 +0600

    Booking confirmation update

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
 
-const BookingRow = ({ booking }) => {
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
 
-    const { img, customerName, service, price, date } = booking;
+    const { img, customerName, service, price, date, _id, status } = booking;
+
+   
 
     return (
         <tr>
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button onClick={() => handleDelete(_id)}
+                    className="btn btn-sm btn-circle btn-outline">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
             </th>
 
             <td>
                 <div className="flex items-center">
                     <div className="avatar">
-                    <div className="rounded h-36 w-36">
-                            <img src={img} alt="Service Image" />
+                        <div className="rounded h-36 w-36">
+                            {
+                                img && <img src={img} alt="Service Image" />
+                            }
                         </div>
                     </div>
                 </div>
@@ -32,7 +48,11 @@ const BookingRow = ({ booking }) => {
 
             <td className='font-medium' >{date}</td>
             <th>
-                <button className="btn bg-[#FF3811] text-white rounded-xl">Delete</button>
+                {
+                    status === 'confirm'?
+                    <span className='font-bold text-primary'>Confirmed</span>:
+                    <button onClick={()=>handleBookingConfirm(_id)} className="btn bg-[#FF3811] text-white rounded-xl">Please Confirm</button>
+                }
             </th>
         </tr>
     )
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -19,7 +19,50 @@ const Bookings = () => {
                 console.log(data)
                 setBookings(data)
             })
-    }, [])
+    }, [url])
+
+    const handleDelete = id => {
+        const proceed = confirm('Are you sure you want to delete');
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: "DELETE",
+
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('delete successful')
+                        const remaining = bookings.filter(booking => booking._id !== id)
+                        setBookings(remaining);
+                    }
+                })
+        }
+    }
+
+    const handleBookingConfirm = id => {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            // ei case e only ekta data pathacchi patch korar jonno , seita holo status. so eita stringify korbo
+            body: JSON.stringify({ status: 'confirm' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    // update state
+                    // alert('update successful')
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking =>booking._id === id);
+                    updated.status ='confirm'
+                    const newBookings = [updated, ...remaining];
+                    setBookings(newBookings);
+                }
+            })
+    }
 
 
     // useEffect(() =>{
@@ -60,6 +103,8 @@ const Bookings = () => {
                                 <BookingRow
                                     key={booking._id}
                                     booking={booking}
+                                    handleDelete={handleDelete}
+                                    handleBookingConfirm={handleBookingConfirm}
                                 ></BookingRow>
                             )
                         }
